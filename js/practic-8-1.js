0263662adfda968df2aa68898a1c8ec3430fa188
Taras Sirenko <tarassirenko71@gmail.com>
2022-09-29 17:44:58 +0300

    connects the main file with links to all tasks

--- js/practic-8-1.js
+++ js/practic-8-1.js
@@ -1,11 +1,11 @@
 // Напиши скрипт который будет делать активным только один элемент коллекции
-// const refs = {
-//   wraperTag: document.querySelector('.wraper-tag'),
-//   itemTag: document.querySelector('.tag-item'),
-//   buttonTag: document.querySelector('.tag-button'),
-// };
+const refs = {
+  wraperTag: document.querySelector('.wraper-tag'),
+  itemTag: document.querySelector('.tag-item'),
+  buttonTag: document.querySelector('.tag-button'),
+};
 
-// refs.wraperTag.addEventListener('click', onTagButtonClick);
+refs.wraperTag.addEventListener('click', onTagButtonClick);
 
 // function onTagButtonClick(event) {
 //   if (event.target.nodeName !== 'BUTTON') {
@@ -14,22 +14,21 @@
 //   let curentActivTag = event.currentTarget.querySelector('.tag-button--active');
 //   curentActivTag?.classList.remove('tag-button--active');
 //   event.target.classList.add('tag-button--active');
-//   console.log(curentActivTag?.dataset.value);
 // }
 
 // А теперь сделай чтобы можно было выбрать много
-// const tagArr = [];
+const tagArr = [];
 
-// function onTagButtonClick(event) {
-//   if (event.target.nodeName !== 'BUTTON') {
-//     return;
-//   }
-//   event.target.classList.toggle('tag-button--active');
-//   if (tagArr.includes(event.target.dataset.value)) {
-//     tagArr.splice(tagArr.indexOf(event.target.dataset.value), 1);
-//   } else {
-//     tagArr.push(event.target.dataset.value);
-//   }
+function onTagButtonClick(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  event.target.classList.toggle('tag-button--active');
+  if (tagArr.includes(event.target.dataset.value)) {
+    tagArr.splice(tagArr.indexOf(event.target.dataset.value), 1);
+  } else {
+    tagArr.push(event.target.dataset.value);
+  }
 
-//   console.log(tagArr);
-// }
+  console.log(tagArr);
+}
